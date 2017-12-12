a8e7302eb1e489e02ae4c77e480471d1de205731
ryosuke <ryosukeaizawa@gmail.com>
2017-12-12 23:23:06 +0900

    up

--- libraries-arduino.ino
+++ libraries-arduino.ino
@@ -1,7 +1,21 @@
-//使えそなメソッドをまとめておく
+//使えそうなメソッドをまとめておく
+
+/* テンプレート ：　行う動作 */
+//変数定義
+void setup() {//setupループ
+}
+void loop(){//メインループ
+}
 
 
-if (1time) { // 1回だけ処理
-  1time = !1time;
-  //~hogehoge~
+
+
+
+/* 1回だけ処理 */
+boolean 1time = true;
+void loop(){
+   if (1time) { // 1回だけ処理
+     1time = !1time;
+     //~hogehoge~
+   }
 }
